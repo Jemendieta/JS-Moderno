470d8ffa822f7d3073e2c896d25f59bfc1b7a638
Jemendieta <mendieta.86@gmail.com>
2024-04-23 18:34:08 -0500

    Update session 01

---
+++ Curso-JS-Moderno/01-introduccion/js/app.js
@@ -0,0 +1,9 @@
+// clase 01 de js
+
+const nombre = prompt("¿Cuál es tu nombre?");
+document.querySelector(
+  ".contenido"
+).innerHTML = `${nombre} Está aprendiendo JavaScript moderno...😜`;
+
+/*Comentario de bloque*/
+// Los comentarios son importantes porque nos permiten ofrecer informción de nuestros proyectos en producción.
